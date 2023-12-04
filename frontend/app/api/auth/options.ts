09c8d43501bc712f69eee526ae612d42dd687e64
Anirudh Patel <anirudh26p@gmail.com>
2023-12-04 18:58:22 +0530

    route.ts next-auth error r1

---
+++ frontend/app/api/auth/options.ts
@@ -0,0 +1,79 @@
+import { AuthOptions } from "next-auth";
+import { Account, User as AuthUser } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import User from "@/config/models/User";
+import connect from "@/config/db";
+import { AdapterUser } from "next-auth/adapters";
+
+export const authOptions: AuthOptions = {
+	providers: [
+		CredentialsProvider({
+			id: "credentials",
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials: any) {
+				await connect();
+				try {
+					const user = await User.findOne({
+						email: credentials.email,
+					});
+					if (user) {
+						const isPasswordCorrect = await bcrypt.compare(
+							credentials.password,
+							user.password
+						);
+						if (isPasswordCorrect) {
+							return user;
+						}
+					}
+				} catch (err: any) {
+					throw new Error(err);
+				}
+			},
+		}),
+		GithubProvider({
+			clientId: process.env.GITHUB_ID ?? "",
+			clientSecret: process.env.GITHUB_SECRET ?? "",
+		}),
+		// ...add more providers here
+	],
+	callbacks: {
+		async signIn({
+			user,
+			account,
+		}: {
+			user: AuthUser | AdapterUser;
+			account: Account | null;
+		}) {
+			if (account?.provider == "credentials") {
+				return true;
+			} else if (account?.provider == "github") {
+				await connect();
+				try {
+					const existingUser = await User.findOne({
+						email: user.email as string,
+					});
+					if (!existingUser) {
+						const newUser = new User({
+							email: user.email as string,
+						});
+
+						await newUser.save();
+						return true;
+					}
+					return true;
+				} catch (err) {
+					console.log("Error saving user", err);
+					return false;
+				}
+			} else {
+				return false;
+			}
+		},
+	},
+};
