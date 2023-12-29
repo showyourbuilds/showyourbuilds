f116e1695126a316bb7fc4a951e49b896fcc8a90
Anirudh Patel <anirudh26p@gmail.com>
2023-12-29 11:50:52 +0530

    [ FIX ] : issues fix

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,32 +1,26 @@
-import User from '@/config/models/User';
-import Project from '@/config/models/Project';
-import { NextResponse } from 'next/server';
-import connect from '@/config/db';
+import User from "@/config/models/User";
+import Project from "@/config/models/Project";
+import { NextResponse } from "next/server";
+import connect from "@/config/db";
 
 export const GET = async (req: any) => {
-    const projects = await getProjects();
-    if (projects) {
-        return NextResponse.json({ projects: projects, status: 200 });
-    } else {
-        return NextResponse.json({ message: "Projects not found", status: 404 });
-    }
-}
-
-async function getProjects() {
-    try{
-        await connect();
-        const projects = await Project.find().populate({
-            path: 'owner',
-            model: User,
-            select: 'image socials username name'
-        }).sort({
-            createdAt: -1,
-            'views.total': -1,
-        });
-
-        return projects;
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
-}
+	await connect();
+	const projects = await Project.find()
+		.populate({
+			path: "owner",
+			model: User,
+			select: "image socials username name",
+		})
+		.sort({
+			createdAt: -1,
+			"views.total": -1,
+		});
+	if (projects.length > 0) {
+		return NextResponse.json({ projects: projects, status: 200 });
+	} else {
+		return NextResponse.json({
+			message: "Projects not found",
+			status: 404,
+		});
+	}
+};
